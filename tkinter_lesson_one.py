fa8729c495673f2b39622dd4538be22e8fae1ce9
Oskar Durys <embern@protonmail.com>
2022-10-13 23:02:52 +0100

    Small changes

--- tkinter_lesson_one.py
+++ tkinter_lesson_one.py
@@ -11,13 +11,13 @@ def bf_on_click(stringvar, window):
         count += 1
 
         press_time = time.time()
-        stringvar.set(str(count))
+        stringvar.set(f"Clicks: {str(count)}")
         
         while True:
             if (press_time - time.time() > 3):
                 print("3 seconds passed")
             window.update()
-            window.update_idletasks()
+            #window.update_idletasks()
 
 def bf_start_timer(stringvar):
     start_time = time.time()
@@ -33,16 +33,16 @@ window = tk.Tk("My Window", None, "Tk", True, False, None)
 window.geometry("500x300")
 window.title("CPS Measurement")
 
-str_count = tk.StringVar(window, "0", "count")
-str_cps = tk.StringVar(window, "Not yet tested", "cps")
+str_count = tk.StringVar(window, "Clicks: 0", "count")
+str_cps = tk.StringVar(window, "CPS: Not yet tested", "cps")
 str_start_time = tk.StringVar(window, "Press to start the timer.", "start_time")
 
 l_click = tk.Label(window, compound=tk.BOTTOM, textvariable=str_count).pack(side=tk.TOP)
-l_cps = tk.Label(window, compound=tk.BOTTOM, textvariable=str_cps).pack(side=tk.TOP)
+l_cps = tk.Label(window, compound=tk.BOTTOM, textvariable=str_cps).pack()
 l_start = tk.Label(window, compound=tk.BOTTOM, textvariable=str_start_time).pack(side=tk.TOP)
 
-b_start = tk.Button(window, textvariable=str_start_time, width=10, height=2,
-                    command=lambda: bf_start_timer(str_start_time)).pack(side=tk.TOP)
+b_start = tk.Button(window, text="Start", width=15, height=3,
+                    command=lambda: bf_start_timer(str_start_time)).pack()
 
 b_click = tk.Button(window, text="Press to increase count", width=20, height=5,
                    command=lambda : bf_on_click(str_count, window)).pack(side=tk.BOTTOM)
